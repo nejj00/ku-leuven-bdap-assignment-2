6c02875eb0c9f60a4b17ae7c3c5d61b960e7dd97
Nedzhib Ahmed <nedjib500@gmail.com>
2026-03-27 13:23:49 +0100

    fix: add mvn package before each run

--- Spark/possession_per_player.sh
+++ Spark/possession_per_player.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Build the project first
+mvn clean package
+
+# Optional: stop if build fails
+if [ $? -ne 0 ]; then
+    echo "Maven build failed. Exiting."
+    exit 1
+fi
+
 MASTER="yarn"
 DATA_DIR="/data/nba_movement_data"
 OUTPUT_DIR="/user/r1035493/output"
